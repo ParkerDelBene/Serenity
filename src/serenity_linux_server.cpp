f04180acd8bae2778d4e3961136a14f884eadebd
Parker DelBene <jcdelbene@gmail.com>
2023-09-02 10:49:26 -0500

    fixing linux server

--- src/serenity_linux_server.cpp
+++ src/serenity_linux_server.cpp
@@ -91,10 +91,10 @@ void *connection_handler(void *socket_desc){
 
     int sock = *(int*)socket_desc;
     int read_size;
-    char message[74], client_message[2000];
+    char message[74] = "Hey, this is the Server speaking. Type me anything and I will repeat it!";
+    char client_message[2000];
 
     //Send a confirmation message to the client
-    message = "Hey, this is the Server speaking. Type me anything and I will repeat it!";
     write(sock, message, strlen(message));
 
     while((read_size = recv(sock, client_message, 2000, 0)) > 0){
